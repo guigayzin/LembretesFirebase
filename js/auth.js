7c2c251574e07e39abf3d536d5dda6c9d2c29268
guigayzin <guilherme.gsantos02@gmail.com>
2026-03-11 16:55:10 -0300

    Integração com firebase auth implementada corretamente

---
+++ js/auth.js
@@ -0,0 +1,20 @@
+authForm.onsubmit = function(event){
+
+    event.preventDefault()
+    if (authForm.submitAuthForm.innerHTML == 'Entrar') {
+        firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).then(function (user) { 
+            
+            console.log('Acessou com sucesso =)')
+            console.log(user)
+
+        }).catch(function (error){
+            console.log('Falha no acesso =(')
+            console.log(error)
+
+        })
+
+    } else {
+
+    }
+
+}
